86dc957aef7743208052ff926668c65bfe507638
reudda <1185398+reudda@users.noreply.github.com>
2022-10-31 17:46:41 +0800

    reudda created a commit :sparkles:

---
+++ cosmos/cosmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cosmosApp.swift
+//  cosmos
+//
+//  Created by mwave on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct cosmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
